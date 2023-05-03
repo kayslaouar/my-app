52f73ffc1c33a3bb5f1cf5f6242effec9f0ee05f
kayslaouar <1kayslaouar@gmail.com>
2023-05-03 16:27:13 -0400

    localhost app complete

---
+++ setup.md
@@ -0,0 +1,10 @@
+### Terminal 1:
+```bash
+cd CS256/my-app
+npm start
+```
+### Terminal 2:
+```bash
+ngrok killall
+ngrok http 3000
+```
--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [direction, setDirection] = useState('');
+
+	const handleClick = dir => {
+		setDirection(dir);
+		axios
+			.post(`http://256finalprojectckm.local/control?direction=${dir}`)
+			.then(response => {
+				console.log(response.data);
+			})
+			.catch(error => {
+				console.log(error);
+			});
+	};
+
+	return (
+		<div className="container">
+			<div className="row justify-content-center align-items-center">
+				<div className="col-auto">
+					<h1>Crane Control</h1>
+					<div className="btn-group-vertical" role="group">
+						<button type="button" className="btn btn-primary" onClick={() => handleClick('down')}>
+							<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-up" viewBox="0 0 16 16">
+								<path
+									fillRule="evenodd"
+									d="M7.5 2a.5.5 0 0 1 .5.5v8.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 0 1 .708-.708L7 11.293V2.5a.5.5 0 0 1 .5-.5z"
+								/>
+							</svg>
+						</button>
+						<button type="button" className="btn btn-primary" onClick={() => handleClick('up')}>
+							<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-down" viewBox="0 0 16 16">
+								<path
+									fillRule="evenodd"
+									d="M7.5 14a.5.5 0 0 0 .5-.5V4.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 0 0 .708.708L7 4.707v8.793a.5.5 0 0 0 .5.5z"
+								/>
+							</svg>
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
